da498fe308df7563793d6f0344f19f3315ffe165
Duc <ducnguyen81103@gmail.com>
2023-09-22 10:45:47 +1000

    flask program

--- Week 2/Learning Flask/flask.py
+++ Week 2/Learning Flask/flask.py
@@ -1,5 +1,6 @@
 #Testing and learning how to use flask to create a website
 #information from https://www.digitalocean.com/community/tutorials/how-to-create-your-first-web-application-using-flask-and-python-3
+#and https://www.youtube.com/watch?v=GHvj1ivQ7ms&t=180s&ab_channel=CodeVoid for virtual environment
 
 from flask import Flask
 
@@ -7,5 +8,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+def index():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
