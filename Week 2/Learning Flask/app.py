8893179290a99d13753dbaa36faabf5879084a87
Duc <ducnguyen81103@gmail.com>
2023-09-22 10:30:41 +1000

    Setting up flask

--- Week 2/Learning Flask/app.py
+++ Week 2/Learning Flask/app.py
@@ -1,3 +1,9 @@
 #Testing and learning how to use flask to create a website
 #information from https://www.digitalocean.com/community/tutorials/how-to-create-your-first-web-application-using-flask-and-python-3
 
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return '<h1>Welcome</h1>'
