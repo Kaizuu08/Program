ba12b1dfa72217dffe12231e17ccd7e33bb57fdc
Duc <ducnguyen81103@gmail.com>
2023-10-07 23:01:15 +1000

    Developing Tkinter Map display
    working on data extraction using panda
    to objectively add markers later

--- Main/Map Integration/Map using Tkinter/file.py
+++ Main/Map Integration/Map using Tkinter/file.py
@@ -1,5 +1,16 @@
 import tkinter
 from tkintermapview import TkinterMapView
+import pandas as pd
+
+# Load the data from the CSV file
+data = pd.read_csv("data.csv")
+
+# Now, you can extract latitude and longitude columns
+latitudes = data["Latitude"]
+longitudes = data["Longitude"]
+
+# Example: Printing the first few rows to verify data loading
+print(data.head())
 
 ## intialising the tkinter window
 root_tk = tkinter.Tk()
@@ -9,5 +20,8 @@ root_tk.title("Google Maps")
 ## displaying the tkintermap widgets
 map_widget = TkinterMapView(root_tk, width = 600, heigh=400, corner_radius=0)
 map_widget.pack(fill="both",expand = True)
+
+## importing google resources
+#map_widget.set_position()
 
 root_tk.mainloop()
