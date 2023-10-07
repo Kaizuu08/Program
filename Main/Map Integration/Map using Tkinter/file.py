b471400dfc0fe42a5fd2885be8ec60c47dc3bc62
Duc <ducnguyen81103@gmail.com>
2023-10-07 23:31:11 +1000

    QOL improvements
    made it upon loading, maps is set in relative location of stations

--- Main/Map Integration/Map using Tkinter/file.py
+++ Main/Map Integration/Map using Tkinter/file.py
@@ -21,8 +21,12 @@ root_tk.geometry("500x500")
 root_tk.title("Google Maps")
 
 ## displaying the tkintermap widgets
-map_widget = TkinterMapView(root_tk, width = 600, heigh=400, corner_radius=0)
+map_widget = TkinterMapView(root_tk, width = 600, height=400, corner_radius=0)
 map_widget.pack(fill="both",expand = True)
+
+## set current widget position and zoom
+map_widget.set_position(locations[0][0], locations[0][1])  # the first station position
+map_widget.set_zoom(15)
 
 ## importing the data as markers
 markers = {}  # Create an empty dictionary to store markers
