51dbe22ebf5bd12e18e6554e23c9ca9e0c3da2a8
Duc <ducnguyen81103@gmail.com>
2023-10-12 22:53:06 +1000

    finished off tkinter map
    added features from google api to calculate distance from 2 points

--- Main/Map Integration/Map using Tkinter/file.py
+++ Main/Map Integration/Map using Tkinter/file.py
@@ -11,8 +11,27 @@ gmaps = googlemaps.Client(key = api_key)
 def calculate_distance():
     origin = origin_var.get()
     destination = destination_var.get()
-    pass
-
+    
+    origin_coords = None
+    destination_coords = None
+    
+    for row in locations:
+        if row[2] == origin:
+            origin_coords = (row[0], row[1])
+        if row[2] == destination:
+            destination_coords = (row[0], row[1])
+            
+    if origin_coords and destination_coords:
+        # Use the Directions API to calculate the distance
+        directions = gmaps.directions(origin_coords, destination_coords, mode="driving")
+        if directions:
+            distance = directions[0]["legs"][0]["distance"]["text"]
+            distance_label.config(text=f"Distance: {distance}")
+        else:
+            distance_label.config(text="Distance calculation failed.")
+    else:
+        distance_label.config(text="Origin or destination not found in the locations data.")
+            
 ## function to zoom into selected location
 def zoom_to_location():
     selected_location = location_var.get()
@@ -67,6 +86,10 @@ destination_dropdown.pack()
 
 calculate_button = tkinter.Button(root_tk, text="Calculate Distance", command=calculate_distance)
 calculate_button.pack()
+
+## Label displaying the calculated distance
+distance_label = tkinter.Label(root_tk, text="")
+distance_label.pack()
 
 ## displaying the tkintermap widgets
 map_widget = TkinterMapView(root_tk, width = 600, height=400, corner_radius=0)
